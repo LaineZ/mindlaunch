3ee2588cd9bb99377b6fcd8f2cb1d19bde62565d
140bpmdubstep <laineprikol@gmail.com>
2020-05-12 16:53:50 +0400

    UI Tweaks: Error if version canot be loaded

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,10 @@ use parking_lot::Mutex;
 use raqote::{
     DrawOptions, DrawTarget, PathBuilder, Point, SolidSource, Source, StrokeStyle, Transform,
 };
+use std::collections::HashMap;
 use std::sync::Arc;
 use std::{io::Error, thread, time};
+use webbrowser;
 
 const WIDTH: usize = 854;
 const HEIGHT: usize = 480;
@@ -27,11 +29,18 @@ struct Release {
     stable: bool,
 }
 
+struct State {
+    version_loading_error: bool
+}
+
 fn main() {
     let mut options = WindowOptions::default();
     options.resize = false;
 
-    let mut versions = Arc::new(Mutex::new(Vec::new()));
+    let versions = Arc::new(Mutex::new(Vec::new()));
+    let state = Arc::new(Mutex::new(State {
+        version_loading_error: false
+    }));
 
     let mut window = Window::new("MindLaunch 1.0", WIDTH, HEIGHT, options).unwrap();
     let font = SystemSource::new()
@@ -44,55 +53,71 @@ fn main() {
     let mut frames: f32 = 0.;
     let mut offset: f32 = 0.0;
     let mut coldown = false;
+    fn load_versions(versions_clone: Arc<Mutex<Vec<Release>>>, state_clone: Arc<Mutex<State>>) {
+        versions_clone.lock().clear();
+        thread::spawn(move || -> Result<(), Error> {
+            //thread::sleep_ms(35000);
+            // get stable releases
+            let data: String =
+                http_tools::http_request("https://api.github.com/repos/Anuken/Mindustry/releases")?;
+            let data_json: Result<Vec<models::release::Root>, serde_json::error::Error> = serde_json::from_str(&data);
+            match data_json {
+                Ok(json) => {
+                    for version in json.iter() {
+                        versions_clone.lock().push(Release {
+                            download_url: version.assets[0].clone().browser_download_url.unwrap(),
+                            installed: false,
+                            name: version
+                                .clone()
+                                .name
+                                .unwrap_or("Unknown version string".to_string()),
+                            stable: true,
+                            tag_name: version.clone().tag_name.unwrap_or("Unknown".to_string()),
+                        });
+                    }
+                }
+                Err(_) => {
+                    state_clone.lock().version_loading_error = true;
+                }
+            }
+            // bleeding edge
+            let data: String = http_tools::http_request(
+                "https://api.github.com/repos/Anuken/MindustryBuilds/releases",
+            )?;
+            let data_json: Vec<models::release::Root> = serde_json::from_str(&data).unwrap();
+            for version in data_json.iter() {
+                versions_clone.lock().push(Release {
+                    download_url: version.assets[0].clone().browser_download_url.unwrap(),
+                    installed: false,
+                    name: version
+                        .clone()
+                        .name
+                        .unwrap_or("Bleeding edge build".to_string()),
+                    stable: false,
+                    tag_name: version.clone().tag_name.unwrap_or("Unknown".to_string()),
+                });
+            }
+            Ok(())
+        });
+    }
 
-    let versions_clone = versions.clone();
+    let mut settings = config::Config::default();
+    settings
+        // Add in `./Settings.toml`
+        .merge(config::File::with_name("Settings")).unwrap()
+        // Add in settings from the environment (with a prefix of APP)
+        // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
+        .merge(config::Environment::with_prefix("APP")).unwrap();
 
-    let loading_thread = thread::spawn(move || -> Result<(), Error> {
-        thread::sleep_ms(35000);
-        // get stable releases
-        let data: String =
-            http_tools::http_request("https://api.github.com/repos/Anuken/Mindustry/releases")?;
-        let data_json: Vec<models::release::Root> = serde_json::from_str(&data).unwrap();
-        for version in data_json.iter() {
-            versions_clone.lock().push(Release {
-                download_url: version.assets[0].clone().browser_download_url.unwrap(),
-                installed: false,
-                name: version
-                    .clone()
-                    .name
-                    .unwrap_or("Unknown version string".to_string()),
-                stable: true,
-                tag_name: version.clone().tag_name.unwrap_or("Unknown".to_string()),
-            });
-        }
-        // bleeding edge
-        let data: String = http_tools::http_request(
-            "https://api.github.com/repos/Anuken/MindustryBuilds/releases",
-        )?;
-        let data_json: Vec<models::release::Root> = serde_json::from_str(&data).unwrap();
-        for version in data_json.iter() {
-            versions_clone.lock().push(Release {
-                download_url: version.assets[0].clone().browser_download_url.unwrap(),
-                installed: false,
-                name: version
-                    .clone()
-                    .name
-                    .unwrap_or("Bleeding edge build".to_string()),
-                stable: false,
-                tag_name: version.clone().tag_name.unwrap_or("Unknown".to_string()),
-            });
-        }
-        Ok(())
-    });
+    let mut settings_hash = settings.try_into::<HashMap<String, String>>().unwrap();
 
-    //let thread_finished = loading_thread.join();
-
+    load_versions(versions.clone(), state.clone());
+    let size = window.get_size();
     // programm processsing
     while window.is_open() {
         let current = time::Instant::now();
         frames += 1.;
         // if resize
-        let size = window.get_size();
         let mut drawing = Drawing::new(DrawTarget::new(size.0 as i32, size.1 as i32), &font);
 
         // event processing
@@ -108,27 +133,83 @@ fn main() {
             }
         });
 
-        //pb.rect(10., 0., 300., 480.);
-
+        // Drawing listbox square
         drawing.draw_square(
             Location4::new(5., 0., 300., 480.),
             Color::new(255, 50, 50, 50),
         );
 
+        // Drawing play buttons
+        {
+            let versions_lock = versions.lock();
+            drawing.draw_text(
+                "PLAY:",
+                Location2::new(320.0, 15.0),
+                Color::new(255, 255, 255, 255),
+                15.0,
+            );
+            if versions_lock.len() > 0 {
+                drawing.draw_button(
+                    &format!("last version {}", versions_lock[0].tag_name),
+                    Location4::new(320.0, 20.0, 200.0, 32.0),
+                    Color::new(255, 100, 120, 100),
+                );
+                {
+                    for version in versions_lock.iter() {
+                        if !version.stable {
+                            drawing.draw_button(
+                                &format!("last bleeding edge {}", version.tag_name),
+                                Location4::new(320.0, 55.0, 200.0, 32.0),
+                                Color::new(255, 120, 100, 100),
+                            );
+                            break;
+                        }
+                    }
+                    drop(versions_lock);
+                }
+            }
+        }
+        // Utils
+        if drawing.draw_button("Reload", Location4::new(310.0, 450.0, 50.0, 24.0), Color::new(255, 100, 110, 100)) {
+            load_versions(versions.clone(), state.clone());
+        }
+        drawing.draw_text("Click here to change Mindustry installation location: ", Location2::new(365.0, 440.0), Color::new(255, 255, 255, 255), 15.0);
+        if drawing.draw_button(&format!("{}", settings_hash.get("path").unwrap()), Location4::new(365.0, 450.0, 460.0, 24.0), Color::new(255, 100, 100, 100)) {
+            let result = nfd::open_pick_folder(None).unwrap_or_else(|e| {
+                panic!(e);
+            });
+    
+            match result {
+                Response::Okay(file_path) => { 
+                    println!("File path = {:?}", file_path);
+                    settings_hash.insert("path".to_string(), file_path);
+            },
+                Response::Cancel => println!("User canceled"),
+                _ => (),
+            }
+        }
+        // Drawing options
+        
+
         // offset of scrllbox
         let mut idx: f32 = -27.0;
 
         if versions.lock().len() <= 0 {
-            if frames >= 30.0 {
+            if state.lock().version_loading_error {
                 drawing.draw_text(
-                    &format!("The download took longer than expected check out console for details"),
-                    Location2::new(10.0, 240.0),
-                    Color::new(255, 255, 255, 255),
-                    8.0,
+                    "Version loading ERROR please try later",
+                    Location2::new(15.0, 240.0),
+                    Color::new(255, 255, 0, 0),
+                    14.0,
                 );
+
+                if drawing.draw_button("Try again", Location4::new(25.0, 260.0, 60.0, 24.0), Color::new(255, 100, 120, 100)) {
+                    load_versions(versions.clone(), state.clone());
+                }
+
             } else {
                 drawing.draw_text(
-                    &format!("Loading versions..."),
+                    "Loading versions...",
                     Location2::new(75.0, 240.0),
                     Color::new(255, 255, 255, 255),
                     15.0,
@@ -159,21 +240,14 @@ fn main() {
 
         // draw scrollbar
         drawing.draw_square(
-            Location4::new(300.0, ((offset - offset * 2.0) / 6.0) % 480.0, 4.0, idx / 8.0),
+            Location4::new(
+                300.0,
+                ((offset - offset * 2.0) / 6.0) % 480.0,
+                4.0,
+                idx / 8.0,
+            ),
             Color::new(255, 110, 110, 110),
         );
-
-        /*
-        let result = nfd::open_pick_folder(None).unwrap_or_else(|e| {
-            panic!(e);
-        });
-
-        match result {
-            Response::Okay(file_path) => println!("File path = {:?}", file_path),
-            Response::Cancel => println!("User canceled"),
-            _ => (),
-        }
-        */
 
         // lock ~60 FPS
         std::thread::sleep(std::time::Duration::from_millis(15));
@@ -194,7 +268,7 @@ fn main() {
         );
 
         window.get_scroll_wheel().map(|scroll| {
-            offset += scroll.1 * 2.0;
+            offset += scroll.1 * 4.0;
         });
 
         let size = window.get_size();
